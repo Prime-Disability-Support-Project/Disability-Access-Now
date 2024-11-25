073ebe1d7c72563362cf053dfb88bc48f8276ac5
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-11-25 14:34:12 -0600

    small edit

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,39 +1,56 @@
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Nav() {
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const unreadAnswersCount = useSelector((store) => store.unreadAnswersCount);
   const [searchTerm, setSearchTerm] = useState('');
-// Handles search form submission
-  const handleSearch = (event) => {
-    event.preventDefault();
 
-  // Put BLOB search-code here!
+  useEffect(() => {
+    if (user.id) {
+      fetchUnreadAnswerCount();
+    }
 
-  console.log('Searching for:', searchTerm)
-  };
+  }, [user.id]);
 
     const fetchUnreadAnswerCount = async () => {
       try {
         const response = await axios.get('/api/questions/user-answered-questions-count')
-        return response.data[0].unread_answered_questions;
+        dispatch({
+          type: 'SET_UNREAD_ANSWERS_COUNT',
+          payload: response.data[0].unread_answered_questions
+        });
       } catch (error) {
         console.error('Error fetching unread answer count:', error)
-        return 0;
+        dispatch({
+          type: 'SET_UNREAD_ANSWERS_COUNT',
+          payload: 0
+        });
       }
     };
 
+    // Handles search form submission
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+  // Put BLOB search-code here!
+  console.log('Searching for:', searchTerm)
+  };
+
+
   return (
     <div className="nav">
+
       {/*links to homepage and navigation title*/}
       <Link to="/home">
         <h2 className="nav-title">Disability Support Application</h2>
       </Link>
+
         {/* If no user is logged in, show these links */}
         {!user.id && (
           // If there's no user, show login/registration links
@@ -61,21 +78,24 @@ function Nav() {
           </button>
         </form>
         </div>
+
             <Link className="navLink" to="/user">
               Home
             </Link>
             
-            <Link className="navLink" to="/unread-answers">
+            <Link className="navLink" to="/user-unread">
             Unread Answers {unreadAnswersCount > 0 && `(${unreadAnswersCount})`}
             </Link>
-            <Link className="navLink" to="/ask">
+
+            <Link className="navLink" to="/new-question-without-article">
               Ask a Question
             </Link>
+
             <div className="dropdown">
               <button className="dropbtn">Resources</button>
               <div className="dropdown-content">
-                <Link to="/am-i-elegible">Am I eligible</Link>
-                <Link to="/forms-you-should-start-with">Forms you should start with</Link>
+                <Link to="/am-i-eligible">Am I eligible</Link>
+                <Link to="/forms-to-should-start-with">Forms to should start with</Link>
                 <Link to="/faqs">FAQs</Link>
                 <Link to="/forms-and-documents">Forms and Documents</Link>
               </div>
@@ -87,10 +107,8 @@ function Nav() {
 
             <LogOutButton className="navLink" />
             </div>
-            
           </>
         )}
-      
     </div>
   );
 }
