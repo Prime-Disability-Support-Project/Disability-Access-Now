e3f7c97e4ce985b13bbd64d517b25a77745d09ff
nick <ndweir13@gmail.com>
2024-11-27 15:18:25 -0600

    edits to saved page

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchPDF from "../Blob/SearchPDF";
+import { useState } from "react";
+
+
+export default function SearchResults(){
+
+
+
+
+
+
+
+    return (
+        <SearchPDF />
+    )
+
+}
