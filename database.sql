6440e51f99500f04ed9e30451de90eccbf2792e8
nick <ndweir13@gmail.com>
2024-11-27 14:52:32 -0600

    removed comma and added clean copy of sql

--- database.sql
+++ database.sql
@@ -27,7 +27,7 @@ CREATE TABLE "articles" (
     "id" SERIAL PRIMARY KEY,               -- auto-incrementing primary key
     "title" VARCHAR(255) NOT NULL,          -- title of the article, max length 255
     "subtitle" VARCHAR(255),                -- subtitle of the article, max length 255
-    "body" TEXT,                            -- body/content of the article (TEXT for flexibility)
+    "body" TEXT                            -- body/content of the article (TEXT for flexibility)
 );
 
 CREATE TABLE "files" (
@@ -54,3 +54,57 @@ CREATE TABLE "articles_files" (
 );
 
 
+
+-- ## CLEAN COPY USE THIS IN POSTICO
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR(1000) NOT NULL,
+    "role" INTEGER DEFAULT 1,
+    "approved" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "questions" (
+    "id" SERIAL PRIMARY KEY,                
+    "question" VARCHAR(1000),                     
+    "answer" VARCHAR(1000),                        
+    "answered" BOOLEAN DEFAULT FALSE,       
+    "unread" BOOLEAN DEFAULT TRUE,          
+    "associated_article_url" VARCHAR(255),  
+    "question_date" DATE,                   
+    "flagged" BOOLEAN DEFAULT FALSE,    
+    "user_id" INTEGER REFERENCES "user"(id) ON DELETE CASCADE 
+);
+
+CREATE TABLE "articles" (
+    "id" SERIAL PRIMARY KEY,              
+    "title" VARCHAR(255) NOT NULL,          
+    "subtitle" VARCHAR(255),                
+    "body" TEXT                            
+);
+
+CREATE TABLE "files" (
+    "id" SERIAL PRIMARY KEY,               
+    "filename" VARCHAR(255) NOT NULL,        
+    "data" BYTEA                            
+);
+
+CREATE TABLE "savedFile" (
+    "id" SERIAL PRIMARY KEY,               
+    "file_id" INT REFERENCES "files" (id) ON DELETE CASCADE NOT NULL,      
+    "user_id" INTEGER REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE "savedArticle" (
+    "id" SERIAL PRIMARY KEY,               
+    "article_id" INT REFERENCES "articles" (id) ON DELETE CASCADE NOT NULL, 
+    "user_id" INTEGER REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE "articles_files" (
+    "id" SERIAL PRIMARY KEY,
+    "article_id" INTEGER REFERENCES "articles"("id") ON DELETE CASCADE,
+    "file_id" INTEGER REFERENCES "files"("id") ON DELETE CASCADE
+    );
