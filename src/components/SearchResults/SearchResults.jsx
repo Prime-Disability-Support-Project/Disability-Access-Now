ffacb285647eefda46f51238a397f6e0f1555268
Evan Bouman <evan.bouman@gmail.com>
2024-12-01 22:43:16 -0600

    Edited search route in files router to also look through article titles. Results returned combines filenames and titles into a "name" column. Also returns a "type" column so the array can be filtered into files and articles. Edited search results page to handle displaying related articles and files separately. Fixed Nav search bar to only push user to search results page if the user isn't already on that page. Also clears the input after submitted. Moved downloadPDF into a utility file so that it could be used in the search results page (and wherever else it is needed).

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import DownloadPDF from '../Blob/DownloadPDF';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { downloadFileHandler } from "../Blob/downloadFile"; // util function for downloading pdf files
 
 const SearchResults = () => {
-  const [searchResults, setSearchResults] = useState([]);
+  const [fileResults, setFileResults] = useState([]);
+  const [articleResults, setArticleResults] = useState([]);
   const location = useLocation();
 
-  const query = new URLSearchParams(location.search).get('query');
+  const query = new URLSearchParams(location.search).get("query");
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       try {
-        const response = await fetch(`http://localhost:5001/api/files/search?keyword=${query}`);
+        const response = await fetch(
+          `http://localhost:5001/api/files/search?keyword=${query}`
+        );
         if (response.ok) {
           const results = await response.json();
-          setSearchResults(results);
+          // searches both files and articles tables
+          // results structure: {id: id, name: filename or title, type: file or article}
+          const files = results.filter((item) => item.type === "file");
+          const articles = results.filter((item) => item.type === "article");
+          setFileResults(files);
+          setArticleResults(articles);
         } else {
-          alert('Error Searching for Files!');
+          alert("Error Searching!");
         }
       } catch (error) {
-        console.error('Unexpected Error Searching for Files:', error);
-        alert('Unexpected Error Searching for Files!');
+        console.error("Unexpected Error Searching:", error);
+        alert("Unexpected Error Searching!");
       }
     };
 
@@ -29,21 +37,37 @@ const SearchResults = () => {
     }
   }, [query]);
 
-
-
   return (
     <div>
       <h2>Search Results for "{query}":</h2>
-      {searchResults.length > 0 ? (
+      <h3>Articles:</h3>
+      {articleResults.length > 0 ? (
         <ul>
-          {searchResults.map((result) => (
+          {articleResults.map((result) => (
             <li key={result.id}>
-              {result.filename} <button onClick={() => DownloadPDF(result.filename)}>Download PDF</button>
+              <a href={`http://localhost:5173/#/articlePage/${result.id}`}>
+                {result.name}
+              </a>
             </li>
           ))}
         </ul>
       ) : (
-        <p>No search results found.</p>
+        <p>No search results found in articles.</p>
+      )}
+      <h3>Files:</h3>
+      {fileResults.length > 0 ? (
+        <ul>
+          {fileResults.map((result) => (
+            <li key={result.id}>
+              {result.name}
+              <button onClick={() => downloadFileHandler(result.name)}>
+                Download PDF
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No search results found in files.</p>
       )}
     </div>
   );
